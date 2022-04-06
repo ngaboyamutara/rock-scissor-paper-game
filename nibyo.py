fe4a0059e449f1a76fc90af7d37a9fbf1e3139ea
ngaboyamutara <103092808+ngaboyamutara@users.noreply.github.com>
2022-04-06 17:07:09 +0800

    Create nibyo.py

---
+++ nibyo.py
@@ -0,0 +1,14 @@
+from tkinter import *
+import random
+
+root = Tk()
+
+root.geometry("600x300")
+
+root.title("Rock Paper Scissor Game")
+
+computer_value = {
+	"0":"Rock",
+	"1":"Paper",
+	"2":"Scissor"
+}
